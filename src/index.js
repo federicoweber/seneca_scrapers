3dbcd4b0d362b32c2c8265877df837da33cf534e
Federico Weber <federicoweber@gmail.com>
2018-01-06 17:27:22 -0800

    scrape amex data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const setup = require('./starter-kit/setup');
-
-const AMEX_URL = 'https://online.americanexpress.com/myca/estmt/us/list.do?intlink=us-ser-soa-accnthub-fin-recentactivity&BPIndex=0&request_type=authreg_Statement&Face=en_US&sorted_index=0';
+const CRED = require('../credentials.json');
 
 exports.handler = async (event, context, callback) => {
   // For keeping the browser launch
@@ -15,6 +14,43 @@ exports.handler = async (event, context, callback) => {
 
 exports.run = async (browser) => {
   const page = await browser.newPage();
-  await page.goto(AMEX_URL);
+  page.on('console', (msg) => console.log('PAGE LOG:', ...msg.args));
+  await page.goto(CRED.url);
+
+  // Login
+  await page.waitFor('#lilo_userName');
+  await page.focus('#lilo_userName');
+  await page.type(CRED.user);
+  await page.focus('#lilo_password');
+  await page.type(CRED.password);
+  await page.click('#lilo_formSubmit');
+
+  // Get transactions
+  await page.waitForNavigation();
+  const transactions = await page.evaluate(() => {
+    let scraped = [];
+    $('#listData .posted-item-body')
+      .each( (id, el) => scraped.push([
+        // date
+        $(el).find('.trans-date-text').data('date'),
+        // checked
+        '',
+        // payee
+        $(el).find('.desc-trans').text(),
+        // category
+        '',
+        // acount
+        'Amex',
+        // amount
+        $(el).find('.colAmount').data('amount') * -1,
+    ]));
+    return scraped;
+  });
+
+  console.log(transactions);
+
+  // done with amex website
+  await page.close();
+
   return 'done';
 };
